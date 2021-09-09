28e1ac5e11b93a80c1b17e48933efd8b2a0cc56d
dhanushkumar2489 <89828263+dhanushkumar2489@users.noreply.github.com>
2021-09-09 17:41:52 +0530

    Add files via upload

---
+++ Day1/ Ternary operator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+		static void Main(string[] args)
+        {
+			int x = 10, y = 100;
+
+			var result = x > y ? "x is greater than y" : "x is less than y";
+
+			Console.WriteLine(result);
+		}
+	}
+
+}
