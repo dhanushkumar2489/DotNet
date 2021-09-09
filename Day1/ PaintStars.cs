d28df4b619a49c939cfdb30a6d73a11813524cbf
dhanushkumar2489 <89828263+dhanushkumar2489@users.noreply.github.com>
2021-09-09 17:46:58 +0530

    Add files via upload

---
+++ Day1/ PaintStars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public void paint(int ab)
+        {
+            for (int i = 0; i < ab; i++)
+            {
+                for (int j = ab-i; j > 0; j--)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.paint(20);
+        }
+    }
+
+}
